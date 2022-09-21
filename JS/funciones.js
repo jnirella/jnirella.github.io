4cb8088f18b2d2656eb2e9e901374120b06e71e3
Jose Nirella <97843845+jnirella@users.noreply.github.com>
2022-09-21 18:13:12 +0200

    dsfsdf

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,7 +1,8 @@
-document.getElementsByClassName("fiddle").style.display="none"; 
+document.getElementsByClassName('fiddle').style.display="none"; 
 function mostrar(clave){
+    var nombre= clave;
     document.getElementsByClassName("fiddle").style.display="none"; 
-    document.getElementById(clave).style.display="block";
+    document.getElementById(nombre).style.display="block";
 }
 function mostrarSimple(){
     document.getElementById("fiddle").innerHTML= '<script async src="//jsfiddle.net/jnirella/muagxnse/73/embed/js,html,css,result/dark/">';
