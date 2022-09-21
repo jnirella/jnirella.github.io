b68c0e63f771cbdf1505000a7e45345ebce5724d
Jose Nirella <97843845+jnirella@users.noreply.github.com>
2022-09-21 19:07:24 +0200

    arreglado final bucle for (espero)

--- JS/funciones.js
+++ JS/funciones.js
@@ -4,12 +4,11 @@ function mostrar(clave){
     var claseFid = document.querySelectorAll('.fiddle');
     /* Se comprueba si el ID del DIV cuadra y se muestra su contenido, sino se oculta el contenido*/
     for(var i=0;i<claseFid.length;i++){
-        var idactual=claseFid[i];
      if (claseFid[i].id == nombre){
-        /*document.getElementById(idactual).style.display="block";*/
+        document.getElementById(claseFid.id).style.display="block";
          alert("se ha mostrado" + claseFid[i].id);
      }else{
-        /*document.getElementById(idactual).style.display="none";*/
+        document.getElementById(claseFid.id).style.display="none";
          alert("se ha borrado"+ claseFid[i].id);
      }           
           }
