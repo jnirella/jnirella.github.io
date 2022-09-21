1be519ea0df3356792f856419446d5378984fdb7
jnirella <jnirella@hotmail.es>
2022-09-21 13:51:14 +0200

    dfdd

--- JS/funciones.js
+++ JS/funciones.js
@@ -2,3 +2,6 @@ function mostrar(clave){
     document.getElementsByClassName("fiddle")=element.style.display = 'none'; 
     document.getElementById(clave)=element.style.display='block';
 }
+function mostrarSimple(){
+    document.getElementById("fiddle")= '<script async src="//jsfiddle.net/jnirella/muagxnse/73/embed/js,html,css,result/dark/">';
+}
