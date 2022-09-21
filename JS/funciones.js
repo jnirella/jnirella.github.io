592d8e37c09c1082e76050fe3884d54457a0d3ae
Jose Nirella <97843845+jnirella@users.noreply.github.com>
2022-09-21 18:03:36 +0200

    arreglo sintaxy

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,7 +1,7 @@
-document.getElementsByClassName("fiddle").style.display = 'none'; 
+document.getElementsByClassName("fiddle").style.display="none"; 
 function mostrar(clave){
-    document.getElementsByClassName("fiddle").style.display = 'none'; 
-    document.getElementById(clave).style.display='block';
+    document.getElementsByClassName("fiddle").style.display="none"; 
+    document.getElementById(clave).style.display="block";
 }
 function mostrarSimple(){
     document.getElementById("fiddle").innerHTML= '<script async src="//jsfiddle.net/jnirella/muagxnse/73/embed/js,html,css,result/dark/">';
