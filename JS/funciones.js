5882f82840099547b9b0966b2b58fac77feeb6bf
Jose Nirella <97843845+jnirella@users.noreply.github.com>
2022-09-21 20:48:53 +0200

    quitadas alertas para debugar

--- JS/funciones.js
+++ JS/funciones.js
@@ -2,29 +2,25 @@
 function mostrar(clave){
     var nombre= clave;
     document.getElementById(nombre).style.display="block";
-    alert("mostrar" + nombre);
 }
 
 /*funcion para ocultar el contenido del div, como parametro el ID*/
 function ocultar(clave){
     var nombre=clave;
     document.getElementById(nombre).style.display="none";
-    alert("ocultar" + nombre);
-}
+ }
 
 /*funcion para comprobar todos los divs de la misma clase y comprobar id por id cual es el correcto para mostrar. los demas se ocultan*/
 function accion(palabra){
     var idDiv=palabra;
     claseFid = document.querySelectorAll('.fiddle');
     
-    for(var i=0;i<claseFid.length;i++){
-        alert("id actual del array" + claseFid[i].id);
+    for(var i=0;i<claseFid.length;i++){ 
      if (claseFid[i].id == idDiv){
          mostrar(idDiv);
      }else{
         ocultar(claseFid[i].id);
          
-     }           
- 
+     } 
  }
 }
