ffac8f9169e5f59d4b5c495b90551f0d76cd341b
Jose Nirella <97843845+jnirella@users.noreply.github.com>
2022-09-21 20:45:46 +0200

    mostrar/ocultar div OK

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,14 +1,18 @@
+/*funcion para mostrar el contenido del div, como parametro el ID*/
 function mostrar(clave){
     var nombre= clave;
     document.getElementById(nombre).style.display="block";
     alert("mostrar" + nombre);
 }
+
+/*funcion para ocultar el contenido del div, como parametro el ID*/
 function ocultar(clave){
     var nombre=clave;
     document.getElementById(nombre).style.display="none";
     alert("ocultar" + nombre);
 }
 
+/*funcion para comprobar todos los divs de la misma clase y comprobar id por id cual es el correcto para mostrar. los demas se ocultan*/
 function accion(palabra){
     var idDiv=palabra;
     claseFid = document.querySelectorAll('.fiddle');
@@ -24,26 +28,3 @@ function accion(palabra){
  
  }
 }
-    /* Se declara variable donde se almacenaran todos los DIV con la class="fiddle"*/
-    
-    /*var claseFid = document.querySelectorAll('.fiddle');*/
-    
-    /* Se comprueba si el ID del DIV cuadra y se muestra su contenido, sino se oculta el contenido*/
-    
-   /* for(var i=0;i<claseFid.length;i++){
-        var idClaseComillas='"'+claseFid[i].id+'"';
-     if (claseFid[i].id == nombre){
-         alert("se ha mostrado " + idClaseComillas);
-         document.getElementById(idClaseComillas).style.display = "block";  
-     }else{
-        document.getElementById(idClaseComillas).style.display = "none";
-         alert("se ha borrado"+ idClaseComillas);
-     }           
- 
- }
- */
-
-
-
-/*prueva para recorrer todos los divs con la clase fiddle y comprobar si su id es la que se entra como parametro en la función se muestra el contenido del div, sino,
-los demas divs se ocultan:*/
