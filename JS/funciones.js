2c2264ba66851c5405bd5e1c2b29d4de29408687
Jose Nirella <97843845+jnirella@users.noreply.github.com>
2022-09-21 20:00:44 +0200

    sigo con el for

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,9 +1,14 @@
 function mostrar(clave){
     var nombre= clave;
+    document.getElementById(nombre).style.display="block";
+    alert(nombre);
     /* Se declara variable donde se almacenaran todos los DIV con la class="fiddle"*/
-    var claseFid = document.querySelectorAll('.fiddle');
+    
+    /*var claseFid = document.querySelectorAll('.fiddle');*/
+    
     /* Se comprueba si el ID del DIV cuadra y se muestra su contenido, sino se oculta el contenido*/
-    for(var i=0;i<claseFid.length;i++){
+    
+   /* for(var i=0;i<claseFid.length;i++){
         var idClaseComillas='"'+claseFid[i].id+'"';
      if (claseFid[i].id == nombre){
          alert("se ha mostrado " + idClaseComillas);
@@ -14,7 +19,7 @@ function mostrar(clave){
      }           
           }
 }
-
+*/
 
 /*prueva para recorrer todos los divs con la clase fiddle y comprobar si su id es la que se entra como parametro en la función se muestra el contenido del div, sino,
 los demas divs se ocultan:*/
